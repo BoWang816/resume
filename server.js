93b9d7b4e9141c699d978e2a80c8e0ae6667d5e6
恪晨 <bo.wang1016@outlook.com>
2021-01-12 18:55:56 +0800

    add koa

---
+++ server.js
@@ -0,0 +1,23 @@
+/**
+ * server.js
+ * @author wangbo
+ * @since 2021/1/12
+ */
+const Koa = require('koa')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+    const server = new Koa()
+    server.use(async (ctx, next) => {
+        await handle(ctx.req, ctx.res)
+        ctx.respond = false
+    })
+
+    server.listen(3000, () => {
+        console.log('server is running at http://localhost:3000')
+    })
+})
